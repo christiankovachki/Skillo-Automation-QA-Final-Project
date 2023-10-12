727c52971783a6193dc511cbf0bdda0f7c6b5d65
Christian Kovachki <christian8251@gmail.com>
2023-10-13 00:32:08 +0300

    Added BasePage

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,4 +1,41 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    protected final String BASE_URL = "http://training.skillo-bg.com:4200";
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+    }
+
+    protected void clickOnElement(WebElement webElement) {
+        waitForElementToBeClickable(webElement);
+        webElement.click();
+    }
+
+    protected void typeInField(WebElement webElement, String input) {
+        waitForVisibilityOfElement(webElement);
+        webElement.sendKeys(input);
+    }
+
+    protected void waitForVisibilityOfElement(WebElement webElement) {
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    protected void waitForElementToBeClickable(WebElement webElement) {
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    protected void waitUrlToBe(String url) {
+        wait.until(ExpectedConditions.urlToBe(url));
+    }
 }

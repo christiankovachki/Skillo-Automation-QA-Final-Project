f45c4cf71a2104ae3e3f8a0ac5a8f829b0945da8
Christian Kovachki <christian8251@gmail.com>
2023-10-15 17:29:48 +0300

    Updated ModalPage class with more Web Elements and Methods

--- src/test/java/pages/ModalPage.java
+++ src/test/java/pages/ModalPage.java
@@ -1,12 +1,121 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class ModalPage extends BasePage {
+    @FindBy(className = "modal-content")
+    private WebElement modal;
+
+    @FindBy(css = "input[placeholder='Comment here']")
+    private WebElement commentField;
+
+    @FindBy(css = ".like.fa-heart")
+    private WebElement heartIcon;
+
+    @FindBy(css = ".fa-heart.liked")
+    private WebElement likedHeartIcon;
+
+    @FindBy(xpath = "//strong[contains(.,' likes')]")
+    private WebElement likesCount;
+
+    @FindBy(css = ".fa-thumbs-down")
+    private WebElement thumbsDownIcon;
+
+    @FindBy(css = ".fa-thumbs-down.liked")
+    private WebElement likedThumbsDownIcon;
+
+    @FindBy(xpath = "//strong[contains(.,' dislikes')]")
+    private WebElement dislikesCount;
+
+    @FindBy(css = ".toast-message[aria-label='Post liked']")
+    private WebElement likedPostToastMessage;
+
+    @FindBy(css = ".toast-message[aria-label='Post disliked']")
+    private WebElement dislikedPostToastMessage;
+
+    @FindBy(css = ".toast-message[aria-label='Post Deleted!']")
+    private WebElement deletedPostToastMessage;
+
+    @FindBy(css = ".delete-ask")
+    private WebElement deleteLink;
+
+    @FindBy(css = "button.btn:first-child")
+    private WebElement deleteConfirmButton;
+
+    @FindBy(css = ".comment-content")
+    private List<WebElement> commentsList;
 
     public ModalPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+    }
+
+    public void waitForVisibilityOfModal() {
+        waitForVisibilityOfElement(modal);
+    }
+
+    public void addComment(String text) {
+        typeInField(commentField, text);
+        commentField.sendKeys(Keys.ENTER);
+    }
+
+    public void likePost() {
+        clickOnElement(heartIcon);
+    }
+
+    public boolean isHeartIconLiked() {
+        waitForVisibilityOfElement(likedHeartIcon);
+
+        return heartIcon.getAttribute("class").contains(" liked");
+    }
+
+    public boolean isThumbsDownIconLiked() {
+        waitForVisibilityOfElement(likedThumbsDownIcon);
+
+        return likedThumbsDownIcon.getAttribute("class").contains("liked");
+    }
+
+    public int getLikesCount() {
+        waitForVisibilityOfElement(likesCount);
+
+        return Integer.parseInt(String.valueOf(likesCount.getText().charAt(0)));
+    }
+
+    public int getDislikesCount() {
+        waitForVisibilityOfElement(dislikesCount);
+
+        return Integer.parseInt(String.valueOf(dislikesCount.getText().charAt(0)));
+    }
+
+    public void dislikePost() {
+        clickOnElement(thumbsDownIcon);
+    }
+
+    public void deletePost() {
+        clickOnElement(deleteLink);
+        clickOnElement(deleteConfirmButton);
+    }
+
+    public String getLikedPostToastMessage() {
+        return getToastMessage(likedPostToastMessage);
+    }
+
+    public String getDislikedPostToastMessage() {
+        return getToastMessage(dislikedPostToastMessage);
+    }
+
+    public String getDeletedPostToastMessage() {
+        return getToastMessage(deletedPostToastMessage);
+    }
+
+    private String getToastMessage(WebElement webElement) {
+        waitForVisibilityOfElement(webElement);
+        return webElement.getText();
     }
 }

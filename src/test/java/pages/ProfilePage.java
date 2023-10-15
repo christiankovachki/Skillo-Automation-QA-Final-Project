700a680dfd3d6d05381a74154a0d9bbcb3f4def8
Christian Kovachki <christian8251@gmail.com>
2023-10-15 17:30:47 +0300

    Updated ProfilePage class

--- src/test/java/pages/ProfilePage.java
+++ src/test/java/pages/ProfilePage.java
@@ -1,12 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class ProfilePage extends BasePage {
-    public final String PROFILE_URL = "http://training.skillo-bg.com:4200/users/";
+    public final String PROFILE_URL = BASE_URL + "/users";
+
+    @FindBy(css = ".gallery-item")
+    private List<WebElement> listOfUsersPosts;
+
     public ProfilePage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+    }
+
+    public ModalPage openLatestPost() {
+        int latestPostIndex = listOfUsersPosts.size() - 1;
+
+        try {
+            clickOnElement(listOfUsersPosts.get(latestPostIndex));
+        } catch (IndexOutOfBoundsException exception) {
+            System.out.println("The User does NOT have any Public Posts!");
+        }
+
+        return new ModalPage(driver);
+    }
+
+    public int getUsersPostsCount() {
+        return listOfUsersPosts.size();
     }
 }
